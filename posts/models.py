0a1d91ff3bd14105a38ef31d2fbed9df7690de17
mido9009 <htjv0000001@gmail.com>
2023-11-07 16:42:56 +0200

    add category

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 
 """
     1 : html widget
@@ -24,13 +25,24 @@ post:
 
 
 class Post(models.Model):
+    auth = models.ForeignKey(User, related_name="post_author", on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=20000)
     draft = models.BooleanField(default=True)
     publish_date = models.DateTimeField(default=timezone.now)
     tags = TaggableManager()
     image = models.ImageField(upload_to="post")
+    category = models.ForeignKey(
+        "Category", related_name="post_category", on_delete=models.SET_NULL, null=True
+    )
     # publish_date2 = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
