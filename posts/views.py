202fa3d4e3adc745b0aa1c560b337e528e64e585
mido9009 <htjv0000001@gmail.com>
2023-11-11 00:48:15 +0200

    add post list

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+
+def list_post(request):
+    data = Post.objects.all()
+    context = {"m": data}
+    return render(request, "posts/list_post.html", context)
+
+
+def post_detial():
+    pass
--- project/urls.py
+++ project/urls.py
@@ -19,8 +19,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from posts.views import list_post, post_detial
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("posts/", list_post),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
